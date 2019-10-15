a5ac194a390d4becac24d2ff4d592efdf5e2effd
Cam Gallucci <cgallucci99@gmail.com>
2019-10-15 19:51:47 -0400

    fixed User tests

--- test/test.js
+++ test/test.js
@@ -1,38 +1,8 @@
 var assert = require('assert');
 var sequelize = require('sequelize');
 var db = require("../models");
-// hello world test
-// describe('Array', function() { // name of test
-//     describe('#indexOf()', function() { // name of function being tested
-//       it('should return -1 when the value is not present', function() { // describe what should happen
-//         assert.equal([1, 2, 3].indexOf(4), -1); // do an assertion for something to happen with specified function
-//       });
-//     });
-//   });
 
 // Test User class
-
-describe('User', function() {
-  describe('#create()', function() {
-    it('should have email equal to test@user.com, firstName equal to test, lastName equal to user, password not equal to password', function() {
-      db.User.destroy({
-        where: {
-          email: 'test@user.com'
-        }
-      });
-      db.User.create({
-        email: 'test@user.com',
-        password: 'password',
-        firstName: 'test',
-        lastName: 'user'
-      }).then(user => {
-        assert.equal(user.email, 'test@user.com');
-        assert.equal(user.firstName, 'test');
-        assert.equal(user.lastName, 'user');
-        assert.notEqual(user.password, 'password');
-      });
-    });
-  });
 
 describe('User', function() {
   describe('#validPassword()', function() {
@@ -54,4 +24,26 @@ describe('User', function() {
       
     });
   });
+  describe('#create()', function() {
+    it('should have email equal to test@user.com, firstName equal to test, lastName equal to user, password not equal to password', function() {
+      db.User.destroy({
+        where: {
+          email: 'test@user.com'
+        }
+      });
+      db.User.create({
+        email: 'test@user.com',
+        password: 'password',
+        firstName: 'test',
+        lastName: 'user'
+      }).then(user => {
+        assert.equal(user.email, 'test@user.com');
+        assert.equal(user.firstName, 'test');
+        assert.equal(user.lastName, 'user');
+        assert.notEqual(user.password, 'password');
+      });
+    });
+  });
 });
+
+// Tests for Books

64cdb5d6602c3c5cbced7b48e6a5fb93fe4d58b9
Cam Gallucci <cgallucci99@gmail.com>
2019-10-29 19:54:33 -0400

    added preliminary book page and route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -23,6 +23,16 @@ module.exports = function (app) {
         });
     });
 
+    app.get("/book/:isbn", function(req, res) {
+        db.Book.findOne({
+            where: {
+                isbn: req.params.isbn
+            }
+        }).then( function(book) {
+            res.render('book', {user: req.user, book: book});
+        })
+    });
+
     app.get("/home/:order", function (req, res) {
         db.Book.findAll({
             order: sequelize.col(req.params.order)
